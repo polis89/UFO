d7849f71a50996af17c5d1d5cec4ef70fed29622
polis89 <89polis@gmail.com>
2016-11-02 20:32:37 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D rb2d;
+
+	public float speed;
+
+	void Start(){
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 vec2 = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (vec2 * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.CompareTag("PickUp")){
+			other.gameObject.SetActive(false);
+		}
+	}
+}
